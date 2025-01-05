fcee198aea125f7d6eef9eac55076c22cdb5b356
ajay <ajaybangar673@gmail.com>
2025-01-06 00:15:48 +0530

    STREAMLIT APP

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import string
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-nltk.download('punkt')
+nltk.download('punkt_tab')
 
 
 ps = PorterStemmer()
